130586e1f4334aed2eef5911cffaefe5abf9a07d
Zwischenablage <github@zwischenablage.org>
2017-10-09 21:09:11 +0200

    used XOR instead since this is the obvous and also much faster solution

--- CompareNumbersWithoutComparing/main.cpp
+++ CompareNumbersWithoutComparing/main.cpp
@@ -8,6 +8,11 @@ int areEqual(int n1, int n2)
 	return ((n1 - n2)*(n1-n2)) ? 1 : 0;
 }
 
+int areEqualXOR(int n1, int n2)
+{
+	return (n1 ^ n2);
+}
+
 int main(int argc, char* argv[])
 {
 	int n1 = 3;
@@ -21,4 +26,10 @@ int main(int argc, char* argv[])
 	n1 = 59, n2 = 59;
 	cout << "Comparing " << to_string(n1) << " with " << to_string(n2) << ": Those numbers are " << (areEqual(n1, n2) ? "NOT " : "") << "equal! " << endl;
 	
+	
+	n1 = 759, n2 = 758;
+	cout << "Comparing " << to_string(n1) << " with " << to_string(n2) << ": Those numbers are " << (areEqualXOR(n1, n2) ? "NOT " : "") << "equal! " << endl;
+	
+	n1 = 759, n2 = 759;
+	cout << "Comparing " << to_string(n1) << " with " << to_string(n2) << ": Those numbers are " << (areEqualXOR(n1, n2) ? "NOT " : "") << "equal! " << endl;
 }
